8c65111dd5c9084243f11b3c622647e664d6c7eb
burak067 <33628360+burak067@users.noreply.github.com>
2017-11-20 15:33:44 +0100

    sql datei

---
+++ WindowsFormsApp1/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id_User] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Email_User] NVARCHAR(50) NOT NULL, 
+    [Name_User] NVARCHAR(50) NOT NULL, 
+    [Vorname_User] NVARCHAR(50) NOT NULL, 
+    [Gdatum_User] NVARCHAR(50) NOT NULL
+)
